6aceb3f13973c391934798b0c3c86a3a7ff58c54
Paul DeLong <pd2375@rtl-lamp-test.cc.columbia.edu>
2013-04-19 22:07:30 -0400

    adding chapter 2 solutions (which only seems to include exercise 8 for some reason);

---
+++ chapter2.lisp
@@ -0,0 +1,53 @@
+#!/usr/bin/clisp
+
+(defun rdots (x)
+   (if
+      (> x 0)
+      (or (format t ".") (rdots (- x 1)))
+      (format t "~%")
+   )
+)
+
+(defun idots (x)
+   (do
+      ((i 1 (+ i 1)))
+      ((> i x) 'done)
+      (format t ".")
+   )
+   (format t "~%")
+)
+
+(rdots 5)
+(idots 5)
+
+(setf foo '(a b c d e a b c a))
+
+;(format t "~A" foo)
+
+(defun rcount (x y)
+   (if
+      (not x)
+      (return-from rcount 0)
+   )
+   (+
+      (if (eql y (car x)) 1 0)
+      (rcount (cdr x) y)
+   )
+)
+
+(defun icount (x y)
+   (let ((n 0))
+      (do
+         ((z x (cdr z)))
+         ((null z) 'done)
+         (if
+            (eql y (car z))
+            (setf n (+ n 1))
+         )
+      )
+      n
+   )
+)
+
+(format t "~A~%" (rcount foo 'a))
+(format t "~A~%" (icount foo 'a))
