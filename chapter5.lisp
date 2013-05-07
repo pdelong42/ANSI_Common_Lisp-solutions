3d3e79ad3a33844ad18085ae186417a29d6582b9
Paul DeLong <Paul.DeLong@gmail.com>
2013-05-06 22:56:41 -0400

    added Exercise 1;

---
+++ chapter5.lisp
@@ -0,0 +1,64 @@
+#!/usr/bin/clisp
+
+(load "util.lisp")
+
+; 5.1
+
+(PrintExercise
+   "Exercise 5.1a - original, with \"let\""
+   ''(let
+      (  (x (car y)))
+      (cons x x)
+   )
+)
+
+(PrintExercise
+   "Exercise 5.1a - translation, sans \"let\""
+   ''(funcall
+      (lambda
+         (x)
+         (cons x x)
+      )
+      (car y)
+   )
+)
+
+(PrintExercise
+   "Exercise 5.1b - original, with \"let\""
+   ''(let*
+      (
+         (w (car x))
+         (y (+ w z))
+      )
+      (cons w y)
+   )
+)
+
+(PrintExercise
+   "Exercise 5.1b - translation #1, sans \"let\""
+   ''(funcall
+      (lambda
+         (w)
+         (funcall
+            (lambda
+               (y)
+               (cons w y)
+            )
+            (+ w z)
+         )
+      )
+      (car x)
+   )
+)
+
+(PrintExercise
+   "Exercise 5.1b - translation #2, sans \"let\""
+   ''(funcall
+      (lambda
+         (w y)
+         (cons w y)
+      )
+      (car x)
+      (+ w z)
+   )
+)
