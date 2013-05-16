f1bc7babe1868eaae3bd948cc97e0beaafdf6d40
Paul DeLong <Paul.DeLong@gmail.com>
2013-05-15 22:58:50 -0400

    added Exercise 4 and the chapter example code it depends upon (needs more testing code);

--- chapter5.lisp
+++ chapter5.lisp
@@ -2,6 +2,185 @@
 
 (load "util.lisp")
 
+; code from Figures 5.1 and 5.2
+
+;(defconstant month
+;   (reverse
+;      (maplist
+;         (lambda
+;            (x)
+;            (apply (function +) x)
+;         )
+;         (reverse
+;            '(0 31 28 31 30 31 30 31 31 30 31 30 31)
+;         )
+;      )
+;   )
+;)
+
+(defconstant month
+   #(0 31 59 90 120 151 181 212 243 273 304 334 365)
+)
+
+(defconstant yzero 2000)
+
+(defun leap?
+   (y)
+   (unless
+      (zerop (mod y 4))
+      (return-from leap?)
+   )
+   (unless
+      (zerop (mod y 100))
+      (return-from leap? t)
+   )
+   (zerop (mod y 400))
+)
+
+(PrintExercise
+   "Figures 5.1 and 5.2 - test of \"leap?\""
+   '(mapcar #'leap? '(1904 1900 1600))
+   '(t nil t)
+)
+
+(defun date->num
+   (d m y)
+   (+
+      (- d 1)
+      (month-num m y)
+      (year-num y)
+   )
+)
+
+(defun month-num
+   (m y)
+   (+
+      (svref month (- m 1))
+      (if
+         (and
+            (> m 2)
+            (leap? y)
+         )
+         1
+         0
+      )
+   )
+)
+
+(defun year-num
+   (y)
+   (let
+      (  (d 0))
+      (if
+         (>= y yzero)
+         (dotimes
+            (i
+               (- y yzero)
+               d
+            )
+            (incf d (year-days (+ yzero i)))
+         )
+         (dotimes
+            (i
+               (- yzero y)
+               (- d)
+            )
+            (incf d (year-days (+ y i)))
+         )
+      )
+   )
+)
+
+(defun year-days
+   (y)
+   (if (leap? y) 366 365)
+)
+
+(defun num->date
+   (n)
+   (multiple-value-bind
+      (y left)
+      (num-year n)
+      (multiple-value-bind
+         (m d)
+         (num-month left y)
+         (values d m y)
+      )
+   )
+)
+
+(defun num-year
+   (n)
+   (if
+      (< n 0)
+      (do*
+         (
+            (y
+               (- yzero 1)
+               (- y 1)
+            )
+            (d
+               (-   (year-days y))
+               (- d (year-days y))
+            )
+         )
+         (
+            (<= d n)
+            (values y (- n d))
+         )
+      )
+      (do*
+         (
+            (y yzero (+ y 1))
+            (prev 0 d)
+         )
+         (
+            (> d n)
+            (values y (- n prev))
+         )
+      )
+   )
+)
+
+
+(defun num-month
+   (n y)
+   (unless
+      (leap? y)
+      (return-from num-month (nmon n))
+   )
+   (cond
+      (
+         (= n 59)
+         (values 2 29)
+      )
+      (
+         (> n 59)
+         (nmon (- n 1))
+      )
+      (t (nmon n))
+   )
+)
+
+(defun nmon
+   (n)
+   (let
+      (  (m (position n month :test #'<)))
+      (values m (+ 1 (- n (svref month (- m 1)))))
+   )
+)
+
+(defun date+
+   (d m y n)
+   (num->date (+ (date->num d m y) n))
+)
+
+(PrintExercise
+   "Figures 5.1 and 5.2 - test of \"date+\""
+   '(multiple-value-list (date+ 17 12 1997 60))
+   '(15 2 1998)
+)
+
 ; 5.1
 
 (PrintExercise
@@ -119,4 +298,34 @@
    "Exercise 5.3"
    '(mapcar #'restricted-square '(-5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9))
    '(25 16 9 4 1 0 1 2 3 4 5 36 49 64 81)
+)
+
+; 5.4
+
+(defun my-month-num
+   (m y)
+   (if
+      (< m 0)
+      (return-from my-month-num 0)
+   )
+   (+
+      (my-month-num (- m 1) y)
+      (case m
+         ((0 2 4 6 7 9 11) 31)
+         ((3 5 8 10) 30)
+         (t (if (leap? y) 29 28))
+      )
+   )
+)
+
+(dolist
+   (y '(2000 2001))
+   (dotimes
+      (i 13)
+      (PrintExercise
+         (format t "Exercise 5.4 - year=~A month=~A~%" y i)
+         `(my-month-num (- ,i 1) ,y)
+         (month-num (+ i 1) y)
+      )
+   )
 )
